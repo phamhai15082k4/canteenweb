2b7c930ac4bb8da1c88898053ece4e0b5942a589
Phạm Hồng Hải <154059771+phamhai15082k4@users.noreply.github.com>
2026-03-15 08:21:26 +0700

    Update LoginSuccessHandler.java

--- canteensystem/src/main/java/com/canteensystem/security/LoginSuccessHandler.java
+++ canteensystem/src/main/java/com/canteensystem/security/LoginSuccessHandler.java
@@ -16,16 +16,16 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        // 1. Lấy danh sách quyền của user vừa đăng nhập
+  
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
         // 2. Kiểm tra quyền và điều hướng
         if (roles.contains("ROLE_ADMIN")) {
-            response.sendRedirect("/admin/dashboard"); // Admin -> Vào Dashboard
+            response.sendRedirect("/admin/dashboard"); 
         } else if (roles.contains("ROLE_STAFF")) {
-            response.sendRedirect("/admin/orders");    // Nhân viên -> Vào trang Đơn hàng
+            response.sendRedirect("/admin/orders");   
         } else {
-            response.sendRedirect("/");                // Sinh viên/Khách -> Về trang chủ
+            response.sendRedirect("/");               
         }
     }
 }
