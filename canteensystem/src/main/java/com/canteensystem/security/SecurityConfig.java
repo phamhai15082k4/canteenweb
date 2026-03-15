1a9fbc615eebcfe370826fd702d26d69cde876e9
Phạm Hồng Hải <154059771+phamhai15082k4@users.noreply.github.com>
2026-03-15 08:59:04 +0700

    Update SecurityConfig.java

--- canteensystem/src/main/java/com/canteensystem/security/SecurityConfig.java
+++ canteensystem/src/main/java/com/canteensystem/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
             )
             .formLogin(form -> form
                 
-                .loginPage("/?login=true") 
+                .loginPage("/login") 
                 .loginProcessingUrl("/perform_login")
                 .successHandler(loginSuccessHandler)
                 .failureUrl("/?error=true") 
